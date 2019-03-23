ce405bc21dcc79c13cec52abe62053a8b7e6a5a7
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-03-23 15:14:37 +0200

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] myArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int i;
+            try
+            {
+                for (i = 0; i <= 9; i++)
+                {
+                    Console.WriteLine(myArray[i]);
+                }
+
+                myArray[3] = int.Parse(Console.ReadLine());
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Exception: Вихід за межі діапазону");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Exception: Format isn't right");
+            }
+        }
+
+    }
+}
